523f3d642854e69f0a424d23b88cb66adc7c9224
kallex <kalle.launiala@protonit.net>
2014-08-26 20:24:20 +0300

    Initial add/edit/view/delete structure for dynamic content

--- WebTemplates/controlpanel_comments_v1/assets/controlpanel/modules/DynamicContentView/DynamicContentViewController.ts
+++ WebTemplates/controlpanel_comments_v1/assets/controlpanel/modules/DynamicContentView/DynamicContentViewController.ts
@@ -45,6 +45,145 @@ class DynamicContentViewController extends ViewControllerBase {
         //alert("Connections view ctrl invisible render: " + this.divID);
     }
 
+    OpenAddDynamicContentModal() {
+        var $modal:any = this.$getNamedFieldWithin("AddNewDynamicContentModal");
+        this.$getNamedFieldWithinModal($modal, "HostName").val("");
+        this.$getNamedFieldWithinModal($modal, "ContentName").val("");
+        this.$getNamedFieldWithinModal($modal, "ElementQuery").val("");
+        this.$getNamedFieldWithinModal($modal, "Description").val("");
+        this.$getNamedFieldWithinModal($modal, "Content").val("");
+        var $content:any = this.$getNamedFieldWithinModal($modal, "Content");
+        $content.redactor(
+            {   minHeight: 300,
+                maxHeight: 350,
+                autoresize: false,
+                buttons: ['bold', 'italic', 'alignment', 'unorderedlist', 'orderedlist', 'image', 'video', "link"]
+            });
+        $modal.foundation('reveal', 'open');
+    }
+
+    EditDynamicContent($source) {
+        var $modal:any = this.$getNamedFieldWithin("EditDynamicContentModal");
+        var me = this;
+        var jq:any = $;
+        var wnd:any = window;
+        var clickedEditID = $source.attr("data-objectid");
+        $.getJSON('../../AaltoGlobalImpact.OIP/DynamicContent/' + clickedEditID + ".json", function (contentData) {
+            var currentObject = contentData;
+            var currentID = currentObject.ID;
+            var currentETag = currentObject.MasterETag;
+            var currentRelativeLocation = currentObject.RelativeLocation;
+            me.$getNamedFieldWithinModal($modal, "ID").val(currentID);
+            me.$getNamedFieldWithinModal($modal, "ETag").val(currentETag);
+            me.$getNamedFieldWithinModal($modal, "RelativeLocation").val(currentRelativeLocation);
+            me.$getNamedFieldWithinModal($modal, "HostName").val(contentData.HostName);
+            me.$getNamedFieldWithinModal($modal, "ContentName").val(contentData.ContentName);
+            me.$getNamedFieldWithinModal($modal, "ElementQuery").val(contentData.ElementQuery);
+            me.$getNamedFieldWithinModal($modal, "Description").val(contentData.Description);
+            me.$getNamedFieldWithinModal($modal, "Content").val(contentData.Content);
+            var $content:any = me.$getNamedFieldWithinModal($modal, "Content");
+            $content.redactor(
+                {   minHeight: 300,
+                    maxHeight: 350,
+                    autoresize: false,
+                    buttons: ['bold', 'italic', 'alignment', 'unorderedlist', 'orderedlist', 'image', 'video', "link"]
+                });
+            $modal.foundation('reveal', 'open');
+        });
+    }
+
+    ViewDynamicContent($source) {
+        var $modal:any = this.$getNamedFieldWithin("ViewDynamicContentModal");
+        var me = this;
+        var jq:any = $;
+        var wnd:any = window;
+        var clickedEditID = $source.attr("data-objectid");
+        $.getJSON('../../AaltoGlobalImpact.OIP/DynamicContent/' + clickedEditID + ".json", function (contentData) {
+            var currentObject = contentData;
+            me.$getNamedFieldWithinModal($modal, "HostName").html(contentData.HostName);
+            me.$getNamedFieldWithinModal($modal, "ContentName").html(contentData.ContentName);
+            me.$getNamedFieldWithinModal($modal, "ElementQuery").html(contentData.ElementQuery);
+            me.$getNamedFieldWithinModal($modal, "Description").html(contentData.Description);
+            me.$getNamedFieldWithinModal($modal, "Content").html(contentData.Content);
+            $modal.foundation('reveal', 'open');
+        });
+    }
+
+    Modal_SaveNewDynamicContent($modal) {
+        var hostName = this.$getNamedFieldWithinModal($modal, "HostName").val();
+        var contentName = this.$getNamedFieldWithinModal($modal, "ContentName").val();
+        var elementQuery = this.$getNamedFieldWithinModal($modal, "ElementQuery").val();
+        var description = this.$getNamedFieldWithinModal($modal, "Description").val();
+        var content = this.$getNamedFieldWithinModal($modal, "Content").val();
+        content = $('<div/>').text(content).html();
+
+        var saveData = {
+            HostName: hostName,
+            ContentName: contentName,
+            ElementQuery: elementQuery,
+            Description: description,
+            "ENC.Content": content
+        };
+
+        var me = this;
+        var jq:any = $;
+        jq.blockUI({ message: '<h2>Adding new dynamic content...</h2>' });
+        me.currOPM.CreateObjectAjax("AaltoGlobalImpact.OIP", "DynamicContent", saveData, function() {
+            setTimeout(function () {
+                jq.unblockUI();
+                $modal.foundation('reveal', 'close');
+                me.ReInitialize();
+            }, 2500);
+        }, me.CommonErrorHandler);
+    }
+
+    Modal_SaveExistingDynamicContent($modal) {
+        var id = this.$getNamedFieldWithinModal($modal, "ID").val();
+        var etag = this.$getNamedFieldWithinModal($modal, "ETag").val();
+        var objectRelativeLocation = this.$getNamedFieldWithinModal($modal, "RelativeLocation").val();
+        var hostName = this.$getNamedFieldWithinModal($modal, "HostName").val();
+        var contentName = this.$getNamedFieldWithinModal($modal, "ContentName").val();
+        var elementQuery = this.$getNamedFieldWithinModal($modal, "ElementQuery").val();
+        var description = this.$getNamedFieldWithinModal($modal, "Description").val();
+        var content = this.$getNamedFieldWithinModal($modal, "Content").val();
+        content = $('<div/>').text(content).html();
+
+        var saveData = {
+            HostName: hostName,
+            ContentName: contentName,
+            ElementQuery: elementQuery,
+            Description: description,
+            "ENC.Content": content
+        };
+
+        var me = this;
+        var jq:any = $;
+        jq.blockUI({ message: '<h2>Saving Dynamic Content...</h2>' });
+        me.currOPM.SaveIndependentObject(id, objectRelativeLocation, etag, saveData, function() {
+            setTimeout(function () {
+                jq.unblockUI();
+                $modal.foundation('reveal', 'close');
+                me.ReInitialize();
+            }, 2500);
+        }, me.CommonErrorHandler);
+    }
+
+    DeleteObject($source) {
+        var id = $source.data("objectid");
+        var domainName = $source.data("domainname");
+        var objectName = $source.data("objectname");
+        var me = this;
+        var jq:any = $;
+        jq.blockUI({ message: '<h2>Deleting...</h2>' });
+        this.currOPM.DeleteIndependentObject(domainName, objectName, id, function(responseData) {
+            setTimeout(function() {
+                jq.unblockUI();
+                me.ReInitialize();
+            }, 2500);
+        });
+    }
+
+
 
 }
 
